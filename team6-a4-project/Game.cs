f81e669639c13e332d14cba0abf82f6f926329bb
James-Belle <jamesbelle549@gmail.com>
2024-11-22 14:12:48 -0500

    Add platform and platform fade

--- team6-a4-project/Game.cs
+++ team6-a4-project/Game.cs
@@ -11,14 +11,18 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
-
+        Platform[] platforms = new Platform[1];
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetSize(800, 600);
 
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                platforms[i] = new Platform();
+            }
         }
 
         /// <summary>
@@ -26,7 +30,18 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-
+            Window.ClearBackground(Color.White);
+            if (Input.IsMouseButtonDown(MouseInput.Left))
+            {
+                foreach (Platform platform in platforms)
+                {
+                    platform.Hitbox(Input.GetMousePosition());
+                }
+            }
+            foreach (Platform platform in platforms)
+            {
+                platform.DrawPlatform();
+            }
         }
     }
 }
---
+++ team6-a4-project/Platform.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+namespace Game10003
+{
+	public class Platform
+	{
+		Vector2 position;
+		bool isTouched = false;
+		Color platGrey = new Color(50, 255);
+		int Timer = 255;
+		public Platform()
+		{
+			position.X = Random.Integer(50, Window.Width - 50);
+			position.Y = 120;
+		}
+
+		public void Spawn()
+		{
+			
+		}
+		public void DrawPlatform()
+		{
+            if (isTouched)
+            {
+                Timer -= 2;
+            }
+            platGrey = new Color(50, Timer);
+            if (Timer < 0)
+            {
+				isTouched = false;
+                Timer = 255;
+            }
+            Vector2 size = new Vector2(100, 20);
+			Draw.FillColor = platGrey;
+			Draw.LineColor = platGrey;
+			Draw.Rectangle(position, size);
+			
+		}
+		public void Hitbox(Vector2 playerPosition)
+		{
+			bool left = playerPosition.X < position.X;
+            bool right = playerPosition.X > position.X + 100;
+
+            bool above = playerPosition.Y < position.Y;
+            bool below = playerPosition.Y > position.Y + 20;
+			if (above && below)
+			{
+				Draw.FillColor = Color.Blue;
+			}
+			if (above)
+			{
+				Draw.FillColor = Color.Green;
+			}
+			else if (below) 
+			{
+				Draw.FillColor = Color.Red;
+			}
+			else
+			{
+				Draw.FillColor = Color.Black;
+			}
+			if (left && right)
+			{
+				Draw.LineColor = Color.Blue;
+			}
+            if (left)
+            {
+                Draw.LineColor = Color.Green;
+            }
+            else if (right)
+            {
+                Draw.LineColor = Color.Red;
+            }
+            else
+            {
+                Draw.LineColor = Color.Black;
+            }
+
+			if (!above && !below && !left && !right)
+			{
+				isTouched = true;
+			}
+            Draw.Circle(position, 20);
+            
+            
+			
+
+        }
+	}
+}
